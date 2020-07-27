fb04cec9b8bfc1232f0299429c301230c86d6fe8
Filippo Valle <filippo.valle@unito.it>
2020-07-27 17:52:21 +0200

    add notebook to test dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="topicpy", # Replace with your own username
-    version="1.0.0",
+    version="1.1.1",
     author="Filippo Valle",
     author_email="filippo.valle@unito.it",
     description="Package to extract information from topic models.",
